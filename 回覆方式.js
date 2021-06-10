04a7b43e539856c7705574aae47b93b0dd111880
Hatsune Miku <39622241+SoraneYuki@users.noreply.github.com>
2021-06-10 15:53:52 +0800

    v20210610

---
+++ 回覆方式.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const TestBot = new Discord.Client();
+
+// 主要訊息觸發地
+
+TestBot.on("message", message =>
+
+    {
+
+        // 下面這就是用Switch來達成收到特定的關鍵詞來觸發
+        // 以下面的為例：
+        // 成員：喵~
+        // Bot：喵～～
+
+        // 但是，當如果成員打"喵"或"喵~~"，都不會觸發
+
+        switch(message.content)
+        {
+
+            case "喵~":
+            message.channel.send("喵～～");
+
+            break;
+
+        }
+
+        
+        
+    }
+
+);
